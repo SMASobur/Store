97029e16f0bc1af87b3975bcdf2044e841368c11
SMASobur <sikdar.se@gmail.com>
2025-05-11 02:07:28 +0200

    Product creation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 dotenv.config();
 
 const app =express()
@@ -9,6 +11,26 @@ const app =express()
 app.get("/", (req, res)=>{
     res.send("server is back here")
 })
+app.use(express.json());
+app.post("/api/products", async (req, res) => {
+	const product = req.body; // user will send this data
+
+	if (!product.name || !product.price || !product.image) {
+		return res.status(400).json({ success: false, message: "Please provide all fields" });
+	}
+
+	const newProduct = new Product(product);
+
+	try {
+		await newProduct.save();
+		res.status(201).json({ success: true, data: newProduct });
+	} catch (error) {
+		console.error("Error in Create product:", error.message);
+		res.status(500).json({ success: false, message: "Server Error" });
+	}
+})
+
+
 
 app.listen(5001, () => {
 	connectDB();
