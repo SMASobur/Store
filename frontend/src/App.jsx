b41644d250bef289ee60680033934149b5c24b57
SMASobur <sikdar.se@gmail.com>
2025-05-11 22:22:28 +0200

    Chakra ui install and setup with test button

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Button, ButtonGroup } from '@chakra-ui/react'
+function App() {
+
+  return (
+    <>
+      <h1>test</h1>
+      <Button colorScheme='blue'>Button</Button>
+    </>
+  )
+}
+
+export default App
