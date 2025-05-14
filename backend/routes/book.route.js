903648a1cf6d8130a11f4b07d13c381daa76cfb7
SMASobur <sikdar.se@gmail.com>
2025-05-15 00:44:01 +0200

    Prepared backend for BookStore

---
+++ backend/routes/book.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { createProductBooks, deleteProductBooks, getProductsBooks, getSingleProductsBooks, updateProductBooks } from "../controllers/book.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProductsBooks);
+router.get("/:id", getSingleProductsBooks);
+router.post("/", createProductBooks);
+router.put("/:id", updateProductBooks);
+router.delete("/:id", deleteProductBooks);
+
+export default router;
