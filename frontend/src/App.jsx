285a05d323a0df1ea1650260edb02f411eb11ece
SMASobur <sikdar.se@gmail.com>
2025-05-18 02:04:49 +0200

    Footer and style fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Box, Divider, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import BookPage from "./pages/BookPage";
 import StoreCardView from "./pages/StoreCardView";
+import AboutPage from "./pages/AboutPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Box minH={"100vh"} bg={useColorModeValue("gray.200", "gray.800")}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-        <Route path="/books" element={<BookPage />} />
-        <Route path="/cards" element={<StoreCardView />} />
-      </Routes>
-    </Box>
+    <>
+      <Box minH={"100vh"} bg={useColorModeValue("gray.200", "gray.800")}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/books" element={<BookPage />} />
+          <Route path="/cards" element={<StoreCardView />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Box>
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,59 +1,72 @@
 import {
+  Box,
   Button,
   Container,
   Flex,
   HStack,
   Text,
   useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { PiBooksDuotone } from "react-icons/pi";
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
 import { BiCreditCardFront } from "react-icons/bi";
+import { MdOutlineDeveloperMode } from "react-icons/md";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Container maxW="fit" px={4}>
-      <Flex
-        h={20}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-        flexDir={{
-          base: "column",
-          sm: "row",
-        }}
-      >
-        <Text
-          fontSize={{ base: "22", sm: "38" }}
-          fontWeight={"bold"}
-          textTransform={"uppercase"}
-          textAlign={"center"}
-          bgGradient={"linear(to-r, orange.400, yellow.400)"}
-          bgClip={"text"}
+    <Box
+      bg={useColorModeValue("gray.300", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container maxW="fit" px={4}>
+        <Flex
+          h={20}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          flexDir={{
+            base: "column",
+            sm: "row",
+          }}
         >
-          <Link to={"/"}> KnitNox</Link>
-        </Text>
+          <Text
+            fontSize={{ base: "22", sm: "38" }}
+            fontWeight={"bold"}
+            textTransform={"uppercase"}
+            textAlign={"center"}
+            bgGradient={"linear(to-r, orange.400, yellow.400)"}
+            bgClip={"text"}
+          >
+            <Link to={"/"}> KnitNox</Link>
+          </Text>
 
-        <HStack spacing={2} alignItems={"center"}>
-          <Link to={"/cards"}>
-            <Button>
-              <BiCreditCardFront fontSize={20} />
+          <HStack spacing={2} alignItems={"center"}>
+            <Link to={"/cards"}>
+              <Button>
+                <BiCreditCardFront fontSize={20} />
+              </Button>
+            </Link>
+            <Link to={"/books"}>
+              <Button>
+                <PiBooksDuotone fontSize={20} />
+              </Button>
+            </Link>
+            <Link to={"/about"}>
+              <Button>
+                <MdOutlineDeveloperMode size={30} />
+              </Button>
+            </Link>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
             </Button>
-          </Link>
-          <Link to={"/books"}>
-            <Button>
-              <PiBooksDuotone fontSize={20} />
-            </Button>
-          </Link>
-          <Button onClick={toggleColorMode}>
-            {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
-          </Button>
-        </HStack>
-      </Flex>
-    </Container>
+          </HStack>
+        </Flex>
+      </Container>
+    </Box>
   );
 };
 export default Navbar;
